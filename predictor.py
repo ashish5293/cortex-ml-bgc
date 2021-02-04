2a5a03178cc60e4fab885bde26609e632dc9b6de
EC2 Default User <ec2-user@ip-172-31-45-210.us-east-2.compute.internal>
2021-02-04 01:48:50 +0000

    update

--- predictor.py
+++ predictor.py
@@ -3,12 +3,9 @@ import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
 import pickle
-from prediction import Prediction
 from pathlib import Path
 import json
 import pandas as pd
-
-labels = ["setosa", "versicolor", "virginica"]
 
 
 class PythonPredictor:
@@ -22,29 +19,27 @@ class PythonPredictor:
         filename = os.path.join(dirname, 'model.pkl')
         self._model = pickle.load(open(filename, "rb"))
    
+   
     def predict(self, payload):
         
         member_id = payload["member_id"]
-        # Load user data directly from redis
-        with open('user-data-redis.txt', 'r') as file:
-            user_data = file.read().replace('\n', '')
-        if user_data is not None:
-            user_data = pd.read_json(user_data)
         
+        # Load user data directly from redis
+        user_data = self.fetch_from_redis(member_id)
 
         if len(user_data.index) < 1:
             print(f'{member_id} has no user interactions')
             return []
-
+        
+        # get predictions
         raw_predictions = self._model.predict(user_data)
 
         if len(raw_predictions.brand.keys()) < 1:
             print(f'{member_id} has no user interactions')
             return []
+            
         raw_predictions = raw_predictions.__dict__
-        print(raw_predictions)
-        print(raw_predictions["brand"].keys())
-        
+
         predictions = list()
         for k in raw_predictions["brand"]:
             predictions.append({
@@ -54,39 +49,13 @@ class PythonPredictor:
                 "gender": raw_predictions["gender"][k],
                 "category_ids": [int(x) for x in raw_predictions["category"][k].split(',')],
             })
-        '''
-        predictions = list(map(
-            lambda k: Prediction(
-                brand_id=raw_predictions["brand"][k],
-                category_ids=[int(x) for x in raw_predictions["category"][k].split(',')],
-                gender=raw_predictions["gender"][k],
-                score=raw_predictions["score"][k],
-                liked=raw_predictions["liked"][k]
-            ), raw_predictions["brand"].keys()))
-        '''
-        print(predictions)
-        print(type(predictions))
+
         return predictions
     
-        
-    '''
-    def __init__(self, config):
-        if os.environ.get("AWS_ACCESS_KEY_ID"):
-            s3 = boto3.client("s3")  # client will use your credentials if available
-        else:
-            s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))  # anonymous client
 
-        s3.download_file(config["bucket"], config["key"], "/tmp/model.pkl")
-        self.model = pickle.load(open("/tmp/model.pkl", "rb"))
-
-    def predict(self, payload):
-        measurements = [
-            payload["sepal_length"],
-            payload["sepal_width"],
-            payload["petal_length"],
-            payload["petal_width"],
-        ]
-
-        label_id = self.model.predict([measurements])[0]
-        return labels[label_id]
-    '''
+    def fetch_from_redis(self, member_id):
+        with open('user-data-redis.txt', 'r') as file:
+            user_data = file.read().replace('\n', '')
+        if user_data is not None:
+            return pd.read_json(user_data)
+        return []
